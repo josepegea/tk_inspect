714e3bcc44d7942bb24f75d2b05354dc4f8c5cd7
Josep Egea <jes@josepegea.com>
2021-01-02 08:27:36 +0100

    Added syntax coloring for classbrowser with rouge gem

--- lib/tk_inspect/class_browser/code_component.rb
+++ lib/tk_inspect/class_browser/code_component.rb
@@ -1,7 +1,32 @@
 require 'active_support/all'
+require 'rouge'
 
 module TkInspect
   module ClassBrowser
+    TOKEN_TAGS = {
+      'Rouge::Token::Tokens::Comment::Single' => :comment,
+      'Rouge::Token::Tokens::Keyword' => :keyword,
+      'Rouge::Token::Tokens::Name::Class' => :constant,
+      'Rouge::Token::Tokens::Name::Constant' => :constant,
+      'Rouge::Token::Tokens::Name::Function' => :method,
+      'Rouge::Token::Tokens::Str::Double' => :string,
+      'Rouge::Token::Tokens::Str::Single' => :string,
+      'Rouge::Token::Tokens::Str::Interpol' => :string,
+      'Rouge::Token::Tokens::Str::Symbol' => :symbol,
+      'Rouge::Token::Tokens::Str::Heredoc' => :string,
+      'Rouge::Token::Tokens::Name::Variable::Instance' => :ivar
+    }
+
+    TAG_STYLES = {
+      string: { foreground: 'red' },
+      comment: { foreground: 'gray' },
+      keyword: { foreground: 'blue' },
+      constant: { foreground: 'green' },
+      symbol: { foreground: 'purple' },
+      method: { foreground: 'brown' },
+      ivar: { foreground: 'brown' }
+    }
+
     class CodeComponent < TkComponent::Base
       attr_accessor :code
       attr_accessor :filename
@@ -17,14 +42,32 @@ module TkInspect
         end
       end
 
+      def component_did_build
+        TAG_STYLES.each do |k,v|
+          @code_text.native_item.tag_configure(k.to_s, v)
+        end
+      end
+
       def update
         if @code && @method_line
-          @code_text.tk_item.value = @code
+          highlight_code(@code_text.native_item, @code, @filename)
           @code_text.tk_item.select_range("#{@method_line}.0", "#{@method_line}.end")
           @code_text.native_item.see("#{@method_line}.0")
           @filename_label.native_item.text(@filename)
         else
+          @filename_label.native_item.text('')
           @code_text.tk_item.value = "Source code not available for #{@method_name}"
+        end
+      end
+
+      @@lexers = {}
+
+      def highlight_code(native_item, code, filename)
+        lexer = (@@lexers[filename] ||= Rouge::Lexers::Ruby.lex(code))
+        native_item.replace('1.0', 'end', '')
+        lexer.each do |token, chunk|
+          tag = TOKEN_TAGS[token.to_s]
+          native_item.insert('end', chunk, tag.to_s)
         end
       end
     end
