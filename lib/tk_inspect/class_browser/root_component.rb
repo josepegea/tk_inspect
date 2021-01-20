cf94063fe5b0574c92c8c587a7047b3689cdf937
Josep Egea <jes@josepegea.com>
2021-01-20 10:42:49 +0100

    Improved layout of ClassBrowser

--- lib/tk_inspect/class_browser/root_component.rb
+++ lib/tk_inspect/class_browser/root_component.rb
@@ -7,16 +7,16 @@ module TkInspect
 
       def generate(parent_component, options = {})
         parse_component(parent_component, options) do |p|
-          p.vframe(padding: "0 0 0 0", sticky: 'nsew', h_weight: 1, v_weight: 1) do |vf|
-            vf.hframe(padding: "0 0 0 0", sticky: 'nsew', h_weight: 1, v_weight: 0) do |hf|
-              hf.hframe(padding: "0 0 0 0", sticky: 'nw') do |f|
+          p.vframe(sticky: 'nsew', h_weight: 1, v_weight: 1) do |vf|
+            vf.hframe(padding: "12", sticky: 'nsew', h_weight: 1, v_weight: 0) do |hf|
+              hf.hframe(sticky: 'nw') do |f|
                 f.label(text: "Browse by: ")
                 f.radio_set(value: "hierarchy", on_change: :browse_by_changed) do |rs|
                   rs.radio_button(text: "Class hierarchy", value: 'hierarchy')
                   rs.radio_button(text: "Namespaces", value: 'namespaces')
                 end
               end
-              hf.hframe(padding: "0 0 0 0", sticky: 'ne') do |f|
+              hf.hframe(sticky: 'ne', h_weight: 1) do |f|
                 f.label(text: "Filter")
                 f.entry(value: class_browser.class_data_source.class_filter, on_change: :filter_changed)
               end
